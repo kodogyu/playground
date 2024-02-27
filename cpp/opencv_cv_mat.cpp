62f93550472c93723f3904a77a3a87ef844a0eb9
kodogyu <kodogyu@khu.ac.kr>
2024-02-27 16:32:34 +0900

    Add cv::Mat basic

--- cpp/opencv_cv_mat.cpp
+++ cpp/opencv_cv_mat.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 int main() {
+    // block read/write
     cv::Mat image = cv::Mat::zeros(3, 3, CV_16UC3);
     image.at<double>(0, 0) = 10;
     // image.at<cv::Vec3s>(0, 0) = cv::Vec3s(10, 10, 10);
@@ -14,7 +15,11 @@ int main() {
     for (int i = 0; i < 3; i++) {
         cout << elem[i] << endl;
     }
-    // cout << image << endl;
+
+    // cv::Mat basics
+    cv::Mat color_image = cv::imread("../ipynb/color_frames/color_image484.png", cv::IMREAD_COLOR);
+    cout << color_image.cols << endl;
+    cout << color_image.dims << endl;
 
     return 0;
 }
