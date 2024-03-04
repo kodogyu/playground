6c88c4c2d327cf1856c0a98ddb4412ee0258d38f
kodogyu <kodogyu@khu.ac.kr>
2024-03-04 23:13:56 +0900

    Edit gtsam_basic file. gtsam::Point3 practice

--- cpp/gtsam_basic.cpp
+++ cpp/gtsam_basic.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <gtsam/linear/NoiseModel.h>
+#include <gtsam/geometry/Pose3.h>
 
 int main() {
-    auto noise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector3(0.3, 0.3, 0.1));
-    auto noise2 = gtsam::noiseModel::Isotropic::Sigma(3, 2);
+    // auto noise = gtsam::noiseModel::Diagonal::Sigmas(gtsam::Vector3(0.3, 0.3, 0.1));
+    // auto noise2 = gtsam::noiseModel::Isotropic::Sigma(3, 2);
 
-    std::cout << noise2->covariance() << std::endl;
+    // std::cout << noise2->covariance() << std::endl;
+
+    gtsam::Point3 v1, v2;
+    v1 << 1, 2, 3;
+    v2 = v1 * 2;
+
+    std::cout << v2 << std::endl;
 
     return 0;
 }
